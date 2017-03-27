267d5826ff2771376e32d19faa61d6568e1e9884
iFlame <iFlame.@mac-mini-2.local>
2017-03-27 13:14:52 +0530

    Initial Commit

---
+++ TableExample/FruitsTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DataTableViewController.swift
+//  TableExample
+//
+//  Created by Ralf Ebert on 19/09/16.
+//  Copyright © 2016 Example. All rights reserved.
+//
+
+import UIKit
+
+class FruitsTableViewController: UITableViewController {
+    
+    var fruits = ["Apple", "Apricot", "Banana", "Blueberry", "Cantaloupe", "Cherry",
+                  "Clementine", "Coconut", "Cranberry", "Fig", "Grape", "Grapefruit",
+                  "Kiwi fruit", "Lemon", "Lime", "Lychee", "Mandarine", "Mango",
+                  "Melon", "Nectarine", "Olive", "Orange", "Papaya", "Peach",
+                  "Pear", "Pineapple", "Raspberry", "Strawberry"]
+    
+    // MARK: - UITableViewDataSource
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+
+        let fruitName = fruits[indexPath.row]
+        cell.textLabel?.text = fruitName
+        cell.detailTextLabel?.text = "Delicious!"
+        cell.imageView?.image = UIImage(named: fruitName)
+        
+        return cell
+    }
+    
+}
